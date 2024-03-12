adcb897d711b9ef077635a1e15b5de3c2911418c
Yuda Yahya <yudayahya7@gmail.com>
2024-03-12 15:17:14 +0700

    create web console/portal include the middleware protection

---
+++ app/Http/Middleware/AuthenticateToWebConsole.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Models\Role;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthenticateToWebConsole
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $guards = empty($guards) ? [null] : $guards;
+
+        foreach ($guards as $guard) {
+            if (Auth::guard($guard)->check()) {
+                if (Auth::user()->hasAnyRole(Role::all())) {
+                    return $next($request);
+                }
+            }
+        }
+
+        return redirect(route('login'));
+    }
+}
