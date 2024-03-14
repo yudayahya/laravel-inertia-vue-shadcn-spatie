76a350f4e95c9b8d0b87bdc2856ae3d97f794b7c
Yuda Yahya <yudayahya7@gmail.com>
2024-03-15 01:54:43 +0700

    provide console dashboard & user view

---
+++ resources/js/Pages/Console/User/View.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AuthenticatedLayout from '@/Layouts/Console/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+</script>
+
+<template>
+    <Head title="User" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="text-2xl font-bold tracking-tight">User</h2>
+        </template>
+
+        <div>This is User Page!</div>
+    </AuthenticatedLayout>
+</template>
