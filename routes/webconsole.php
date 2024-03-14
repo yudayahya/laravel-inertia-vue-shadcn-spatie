a75e4335998be4c2bd50170b77ec6021f3be08bd
Yuda Yahya <yudayahya7@gmail.com>
2024-03-15 01:51:14 +0700

    add user route

--- routes/webconsole.php
+++ routes/webconsole.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return 'Welcome to the console!';
-});
+    return Inertia::render('Console/Dashboard');
+})->name('console.dashboard');
+
+Route::get('/user', function () {
+    return Inertia::render('Console/User/View');
+})->name('console.user.view');
